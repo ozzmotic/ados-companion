1fc23aaeb7ffef87705a6561b7e9ba0fefc28ba8
ozzmotic <ozzmotic@gmail.com>
2015-05-18 10:41:56 -0400

    Add toggle buttons, event list for ADOS module 1

--- app/src/main/java/goodwin/ados/adoscompanion/MainActivity.java
+++ app/src/main/java/goodwin/ados/adoscompanion/MainActivity.java
@@ -5,11 +5,15 @@ import android.os.SystemClock;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.util.Pair;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Chronometer;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.HapticFeedbackConstants;
+import android.view.View.OnTouchListener;
+import android.view.MotionEvent;
+import android.widget.ToggleButton;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -30,6 +34,19 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initializeButton( R.id.start_session, HapticFeedbackConstants.KEYBOARD_TAP );
+        initializeButton( R.id.end_session, HapticFeedbackConstants.KEYBOARD_TAP );
+
+        //Event list
+        int[] buttonIDs = new int[] {R.id.freeplay,R.id.name,R.id.rabbit_car,R.id.bubbles,
+            R.id.balloon,R.id.social_smile,R.id.peek_a_boo,R.id.imitation,R.id.birthday,R.id.snack,
+            R.id.ec_examiner,R.id.ec_parent,R.id.rss,R.id.directX,R.id.gesture,R.id.requesting,
+            R.id.init_JA,R.id.eye_gaze,R.id.language,R.id.sensory,R.id.rep,R.id.anx};
+
+        for (int id : buttonIDs) {
+            initializeButton( id, HapticFeedbackConstants.KEYBOARD_TAP );
+        }
+
         eventLog = new ArrayList<>();
         mChronometer = (Chronometer) findViewById(R.id.chronometer);
     }
@@ -43,6 +60,8 @@ public class MainActivity extends ActionBarActivity {
     public long getCurrentTime() {
         return SystemClock.elapsedRealtime() - mChronometer.getBase();
     }
+
+    //TODO: Pause button
 
     public void stopTimer(View v) {
         mChronometer.stop();
@@ -66,7 +85,25 @@ public class MainActivity extends ActionBarActivity {
     public void logEvent(View v) {
         String event = (String)v.getTag();
         String time = String.valueOf(getCurrentTime());
+        boolean on = ((ToggleButton) v).isChecked();
+
+        if (on) {
+            eventLog.add(new String[] {event + "_started", time});
+        } else {
+            eventLog.add(new String[] {event + "_ended", time});
+        }
+
+    }
+
+    public void logBehavior(View v) {
+        String event = (String)v.getTag();
+        String time = String.valueOf(getCurrentTime());
         eventLog.add(new String[] {event, time});
+    }
+
+    private void initializeButton(int btnId, int hapticId) {
+        Button btn = ( Button ) findViewById( btnId );
+        btn.setOnTouchListener( new HapticTouchListener(hapticId) );
     }
 
     @Override
@@ -89,5 +126,25 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private class HapticTouchListener implements OnTouchListener {
+
+        private final int feedbackType;
+
+        public HapticTouchListener( int type ) { feedbackType = type; }
+
+        public int feedbackType() { return feedbackType; }
+
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            // only perform feedback when the user touches the view, as opposed
+            // to lifting a finger off the view
+            if( event.getAction() == MotionEvent.ACTION_DOWN ){
+                // perform the feedback
+                v.performHapticFeedback( feedbackType() );
+            }
+            return true;
+        }
     }
 }
